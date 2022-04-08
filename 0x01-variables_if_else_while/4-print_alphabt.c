065d159d5de3a3363a3d769bc8077e6cb7316f42
sami714 <eshetemenassie714@gmail.com>
2022-04-08 11:38:59 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Description: Print lowercasealphabet excluding e and q
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c != 'e' && c != 'q')
+
+			putchar(c);
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
+
+
