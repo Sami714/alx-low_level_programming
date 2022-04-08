26b2a9dbce1cde130a127d76ade9551fcda1520d
sami714 <eshetemenassie714@gmail.com>
2022-04-08 08:15:43 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int c;
+int c;
 
-	for (c = 0; c < 26; ++c)
-		putchar('a' +c);
+for (c = 0; c < 26; ++c)
+putchar('a' +c);
 
-	putchar('\n');
+putchar('\n');
 	
-	return (0);
+return (0);
 }
