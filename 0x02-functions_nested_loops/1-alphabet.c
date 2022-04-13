646fa184dac1d96094f1dc813c0c21d47e10d637
sami714 <eshetemenassie714@gmail.com>
2022-04-12 18:54:21 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line 
  */
-void print_:alphabet(void)
+void print_alphabet(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
