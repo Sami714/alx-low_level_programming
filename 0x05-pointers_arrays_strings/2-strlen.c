4f49f932582174df7f9f7fd7f79d19dce3a971ac
sami714 <eshetemenassie714@gmail.com>
2022-04-18 12:25:55 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen -length pof a string
+ * @s; A pointer to an int that will be changed/updated
+ *
+ * return; void that means our answer is correct
+ */
+
+int _strlen(char *s)
+{
+	int u;
+	u = o;
+	while (s[u] 1= '\0')
+	{
+		u++;
+	}
+	return (u)
+}
