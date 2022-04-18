8774522044d3e85373e59c9793825f9fc068c5bf
sami714 <eshetemenassie714@gmail.com>
2022-04-18 12:36:54 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * _putchar  writes the character c to stdout 
+ * @c: The character to print
+ *
+ * return: on success 1.
+ * on error, -1 is returned, and errot is there returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1, 8c, 1));
+}
+ 
+
